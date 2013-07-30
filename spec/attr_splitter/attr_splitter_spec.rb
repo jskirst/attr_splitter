58986a1041090abd00325a456af35ab22823939a
Jonathan Kirst <jc@blueigloo.us>
2013-07-29 21:58:58 -0700

    Adding formbuilderaddition tests.

--- spec/attr_splitter/attr_splitter_spec.rb
+++ spec/attr_splitter/attr_splitter_spec.rb
@@ -6,6 +6,8 @@ class User < SuperModel::Base
   
   attr_splitter :phone_number, prefixes: [:first, :second, :third]
   attr_splitter :home_phone, suffixes: [:area_code, :first_three, :last_four]
+  
+  attr_accessor :phone_number, :home_phone
 end
 
 describe AttrSplitter::ModelAdditions do
@@ -21,17 +23,55 @@ describe AttrSplitter::ModelAdditions do
   end
 end
 
-# describe AttrSplitter::FormBuilderAdditions do
-#   include AttrSplitterSpecHelper
-#   
-#   before :each do
-#     @output_buffer = ''
-#   end
-#   
-#   it "outputs something" do
-#     concat(form_for(User.new, url: "/test") do |f|
-#       concat(f.multi_text_field(:phone_number, fields: ["first", "second", "third"]))
-#     end)
-#   end
-# end
+describe AttrSplitter::FormBuilderAdditions do  
+  describe "with prefixed output" do
+    describe "without jump" do
+      it "should output three prefixed fields" do
+        template = ActionView::Base.new
+        template.output_buffer = ""
+        builder = ActionView::Helpers::FormBuilder.new(:user, 
+          User.new, 
+          template, 
+          {}, 
+          proc { })
+        output = builder.multi_text_field(:phone_number, prefixes: { first: 3, second: 3, third: 4 })
+        output.should include(%q{<input id="user_first_phone_number" maxlength="3" name="user[first_phone_number]" onkeyup="" style="width: 1.875em; margin-right: 5px;" type="text" />})
+        output.should include(%q{<input id="user_second_phone_number" maxlength="3" name="user[second_phone_number]" onkeyup="" style="width: 1.875em; margin-right: 5px;" type="text" />})
+        output.should include(%q{<input id="user_third_phone_number" maxlength="4" name="user[third_phone_number]" onkeyup="" style="width: 2.5em; margin-right: 5px;" type="text" />})
+      end
+    end
+    
+    describe "with jump" do
+      it "should output three prefixed fields with jump javascript" do
+        template = ActionView::Base.new
+        template.output_buffer = ""
+        builder = ActionView::Helpers::FormBuilder.new(:user, 
+          User.new, 
+          template, 
+          {}, 
+          proc { })
+        output = builder.multi_text_field(:phone_number, prefixes: { first: 3, second: 3, third: 4 }, include_jump: true)
+        output.should include(%q{<input id="user_first_phone_number" maxlength="3" name="user[first_phone_number]" onkeyup="if(this.value.length == this.maxLength){ document.getElementById(&#x27;user_second_phone_number&#x27;).focus();}" style="width: 1.875em; margin-right: 5px;" type="text" />})
+        output.should include(%q{<input id="user_second_phone_number" maxlength="3" name="user[second_phone_number]" onkeyup="if(this.value.length == this.maxLength){ document.getElementById(&#x27;user_third_phone_number&#x27;).focus();}" style="width: 1.875em; margin-right: 5px;" type="text" />})
+        output.should include(%q{<input id="user_third_phone_number" maxlength="4" name="user[third_phone_number]" onkeyup="" style="width: 2.5em; margin-right: 5px;" type="text" />})
+      end
+    end
+  end
+  
+  describe "with suffixed output" do
+    it "should output three suffixed fields" do
+      template = ActionView::Base.new
+      template.output_buffer = ""
+      builder = ActionView::Helpers::FormBuilder.new(:user, 
+        User.new, 
+        template, 
+        {}, 
+        proc { })
+      output = builder.multi_text_field(:home_phone, suffixes: { area_code: 3, first_three: 3, last_four: 4 })
+      output.should include(%q{<input id="user_home_phone_area_code" maxlength="3" name="user[home_phone_area_code]" onkeyup="" style="width: 1.875em; margin-right: 5px;" type="text" />})
+      output.should include(%q{<input id="user_home_phone_first_three" maxlength="3" name="user[home_phone_first_three]" onkeyup="" style="width: 1.875em; margin-right: 5px;" type="text" />})
+      output.should include(%q{<input id="user_home_phone_last_four" maxlength="4" name="user[home_phone_last_four]" onkeyup="" style="width: 2.5em; margin-right: 5px;" type="text" />})
+    end
+  end
+end
   
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,43 +6,4 @@ require 'action_view'
 require 'action_controller'
 require 'action_dispatch'
 
-module FakeHelpersModule
-end
-
-module AttrSplitterSpecHelper
-  include ActionPack
-  include ActionView::Context if defined?(ActionView::Context)
-  include ActionController::RecordIdentifier if defined?(ActionController::RecordIdentifier)
-  include ActionView::Helpers::FormHelper
-  include ActionView::Helpers::FormTagHelper
-  include ActionView::Helpers::FormOptionsHelper
-  include ActionView::Helpers::UrlHelper
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::ActiveRecordHelper if defined?(ActionView::Helpers::ActiveRecordHelper)
-  include ActionView::Helpers::ActiveModelHelper if defined?(ActionView::Helpers::ActiveModelHelper)
-  include ActionView::Helpers::DateHelper
-  include ActionView::Helpers::CaptureHelper
-  include ActionView::Helpers::AssetTagHelper
-  include ActiveSupport
-  include ActionController::PolymorphicRoutes if defined?(ActionController::PolymorphicRoutes)
-  include ActionDispatch::Routing::PolymorphicRoutes 
-  include AbstractController::UrlFor if defined?(AbstractController::UrlFor)
-  include ActionView::RecordIdentifier if defined?(ActionView::RecordIdentifier)
-  
-  include AttrSplitter::FormBuilderAdditions
-  
-  def self.included(base)
-    base.class_eval do
-      attr_accessor :output_buffer
-      def protect_against_forgery?; false; end
-      def _helpers
-        FakeHelpersModule
-      end
-    end
-  end
-end
-# 
-# # Mocking forms will fail unless we override this
-# def protect_against_forgery?
-# end
+ActionView::Helpers::FormBuilder.send :include, AttrSplitter::FormBuilderAdditions
